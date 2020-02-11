751176ec72ce5c8046434744819df14a4d5d635c
Sergi <srubio@cells.es>
2020-02-11 11:48:32 +0100

    add NTasks attributes, add push events

--- ProcessProfiler.py
+++ ProcessProfiler.py
@@ -37,7 +37,7 @@ import sys,os,traceback,re,time
 #from multiprocessing import cpu_count#,Process
 #import threading
 
-import fandango
+import fandango as fn
 from fandango.linos import shell_command,get_process_pid,get_memory #,get_cpu,get_memory
 
 if 'PyDeviceClass' not in dir(PyTango): PyTango.PyDeviceClass = PyTango.DeviceClass
@@ -57,37 +57,37 @@ __doc__ = """
     A sample code to do so would be::
 
         import fandango
-        host = fandango.linos.MyMachine().host
+        host = fn.linos.MyMachine().host
         #Create the device
-        fandango.tango.add_new_device('ProcessProfiler/%s'%host,'ProcessProfiler','sys/profile/%s'%host)
+        fn.tango.add_new_device('ProcessProfiler/%s'%host,'ProcessProfiler','sys/profile/%s'%host)
         #Setup properties
-        fandango.get_database().put_device_property('sys/profile/%s'%host,{'ProcessList':['Starter','notifd','ProcessProfiler']})
+        fn.get_database().put_device_property('sys/profile/%s'%host,{'ProcessList':['Starter','notifd','ProcessProfiler']})
         #Start the device
-        fandango.Astor('sys/profile/%s'%host).start_servers(host=host)
+        fn.Astor('sys/profile/%s'%host).start_servers(host=host)
         #Start the update() command polling
         time.sleep(15.)
-        fandango.get_device(sys/profile/%s'%host).poll_command('update',10000)
+        fn.get_device(sys/profile/%s'%host).poll_command('update',10000)
     """
 
 #def get_nthreads(process_pid):
 #    comm = "ps -ef |grep %d|grep -v grep |awk '{print $4}'"%process_pid
-#    threads_list = fandango.linos.shell_command(comm).split('\n')
+#    threads_list = shell_command(comm).split('\n')
 #    return len(threads_list)
 #
 #def get_cpu(pid):
 #    """ Uses ps to get the CPU usage of a process by PID ; it will trigger exception of PID doesn't exist """
 #    comm = 'ps h -p %d -o pcpu'%pid
-#    cpu = fandango.linos.shell_command(comm)
+#    cpu = shell_command(comm)
 #    return float(cpu)
 #def get_memory(pid):
 #    comm = 'ps h -p %d -o size'%pid
-#    cpu = fandango.linos.shell_command(comm)
+#    cpu = shell_command(comm)
 #    return float(cpu)/1000 #MB
 
 #def get_threads_cpu_and_memory(pid):
     #try:
         #comm = 'ps h -p %d -o nlwp,%%cpu,%%mem,rss,vsize'%pid
-        #bar = filter(bool,fandango.linos.shell_command(comm).strip().split(' '))
+        #bar = filter(bool,shell_command(comm).strip().split(' '))
         #nthreads,cpu,memRatio,mem,vmem = bar
         #return [int(nthreads),float(cpu),float(memRatio),float(mem)/1000.,float(vmem)/1000.] #MB
     #except:
@@ -96,14 +96,14 @@ __doc__ = """
 #def get_processes_pid(process_name):
     #try:
         #comm = "ps -ef |grep '%s'|grep -v grep |awk '{print $2}'"%process_name
-        #process_list = fandango.linos.shell_command(comm)
+        #process_list = shell_command(comm)
         #if process_list == None: return []
         #return [int(p) for p in process_list.split('\n') if p.strip()]
     #except:
         #return []
         
 def tracer(msg):
-    print('%s: %s'%(fandango.time2str(),msg))
+    print('%s: %s'%(fn.time2str(),msg))
         
 def memory_checker(f):
     def wrapper(*args,**kwargs):
@@ -126,7 +126,7 @@ def get_all_process(regexp=''):
     pss = {}
     try:
         comm = "ps hax -o pid,nlwp,%cpu,%mem,rss,vsize,cmd"#hax is needed to show Starter process
-        lines = fandango.linos.shell_command(comm).split('\n')
+        lines = shell_command(comm).split('\n')
         for r in map(str.split,lines):
             if not r: continue
             r = dict((k,v if k=='cmd' else (float(v) if '.' in v else int(v))) for k,v in zip(('pid','threads','cpu','mem','rss','vsize','cmd'),(r[0:6])+[' '.join(r[6:])]))
@@ -171,6 +171,8 @@ class ProcessProfiler(PyTango.Device_4Impl):
             meminfo = dict(map(str.strip,a.split(':',1)) for a in meminfo if a.strip())
             getter = lambda k: float(eval(meminfo[k].lower().replace('kb','*1').replace('mb','*1e3').replace('b','*1e-3')))
             self._total, self._free, self._cached = getter('MemTotal'),getter('MemFree'),getter('Cached')
+            ## Shared Memory Reclaimable could be considered also as Cached, but it is not 100% sure if it will be always reusable or not
+            # Therefore, I prefered to ignore it. This is why this method may return less free memory than top.
             self._used = (self._total-(self._free+self._cached))
             self._memrate = (self._used/self._total)
         except:
@@ -187,9 +189,11 @@ class ProcessProfiler(PyTango.Device_4Impl):
             try:
                 #self.debug_stream("In %s::fireEventsList() attribute: %s"%(self.get_name(),attrEvent[0]))
                 if len(attrEvent) == 3:#specifies quality
-                    self.push_change_event(attrEvent[0],attrEvent[1],timestamp,attrEvent[2])
+                    self.push_change_event(attrEvent[0],attrEvent[1],
+                                    timestamp,attrEvent[2])
                 else:
-                    self.push_change_event(attrEvent[0],attrEvent[1],timestamp,PyTango.AttrQuality.ATTR_VALID)
+                    self.push_change_event(attrEvent[0],attrEvent[1],
+                                    timestamp,PyTango.AttrQuality.ATTR_VALID)
             except Exception,e:
                 self.error_stream("In %s::fireEventsList() Exception with attribute %s"%(self.get_name(),attrEvent[0]))
                 print e
@@ -256,14 +260,17 @@ class ProcessProfiler(PyTango.Device_4Impl):
                                                     PyTango.AttrWriteType.READ),
                                        self.read_dyn_attr,None,
                                        (lambda s,req_type,attr_name=aname+'_'+suffix: True))
-                    if self.UseEvents: self.set_change_event(aname+'_'+suffix, True, False)
+                    if self.UseEvents: 
+                        self.set_change_event(aname+'_'+suffix, True, False)
+
                 elif data_format == PyTango.SPECTRUM:
                     self.add_attribute(PyTango.SpectrumAttr(aname+'_'+suffix,
                                                             data_type,
                                                             PyTango.READ,1000),
                                        self.read_dyn_attr,None,
                                        (lambda s,req_type,attr_name=aname+'_'+suffix: True))
-                    if self.UseEvents: self.set_change_event(aname+'_'+suffix, True, False)
+                    if self.UseEvents: 
+                        self.set_change_event(aname+'_'+suffix, True, False)
         #self._dyn_attr_build = True
         self.change_state(PyTango.DevState.ON)
         return
@@ -275,7 +282,7 @@ class ProcessProfiler(PyTango.Device_4Impl):
         PyTango.Device_4Impl.__init__(self,cl,name)
         #self._dyn_attr_build = False
         ProcessProfiler.init_device(self)
-        #self.log = fandango.Logger('ProcessProfiler/%s'%name)
+        #self.log = fn.Logger('ProcessProfiler/%s'%name)
         #self.info_stream = self.log.info
         #self.warning_stream = self.error_stream = self.log.warning
 
@@ -296,9 +303,9 @@ class ProcessProfiler(PyTango.Device_4Impl):
         self.get_device_properties(self.get_device_class())
         
         if self.UseEvents:
-            self.set_change_event('State', True, False)
-            self.set_change_event('Status', True, False)
-            self.set_change_event('LoadAverage', True, False)
+            for a in ['State','Status','LoadAverage','MaxRss','MaxRssProcess',
+                      'MemUsage','MemRate','NTasks','UpdateLapseTime']:
+                self.set_change_event(a, True, False)
         
         self._important_logs = []
         self._loadAverage = []
@@ -310,6 +317,8 @@ class ProcessProfiler(PyTango.Device_4Impl):
         self._memrate = 0.
         self.updateThread = None
         self.lapseTime = None
+        self.maxrss, self.maxrsspid, self.maxrssname = 0,0,''
+        self._total, self._free, self._cached, self._used = 1,0,0,0
         print 'Ready to receive request ...\n\n'
         self.dyn_attr()#necessary to re-populate dynamic attributes if the property list have changed and device Init().
         
@@ -392,7 +401,7 @@ class ProcessProfiler(PyTango.Device_4Impl):
 #------------------------------------------------------------------
 #    Read MemUsage attribute
 #------------------------------------------------------------------
-    def read_MemUsage(self, attr):
+    def read_MemUsage(self, attr=None):
         #self.debug_stream("In read_MemUsage()")
         
         #    Add your own code here
@@ -400,13 +409,29 @@ class ProcessProfiler(PyTango.Device_4Impl):
             v = self.all_proc[self._PID]['rss']
         else:
             v = self.getMemUsage()
-        attr.set_value(v)
+        if attr is not None:
+            attr.set_value(v)
+        return v
+        
+#------------------------------------------------------------------
+#    Read MemRate attribute
+#------------------------------------------------------------------        
         
     def read_MemRate(self, attr):
-        #self.debug_stream("In read_MemUsage()")
+        #self.debug_stream("In read_MemRate()")
         
         #    Add your own code here
         attr.set_value(self._memrate)
+        
+#------------------------------------------------------------------
+#    Read NTasks attribute
+#------------------------------------------------------------------        
+        
+    def read_NTasks(self, attr):
+        #self.debug_stream("In read_NTasks()")
+        
+        #    Add your own code here
+        attr.set_value(self._nTasks)
 
 #==================================================================
 #
@@ -430,6 +455,12 @@ class ProcessProfiler(PyTango.Device_4Impl):
                 #self.warn_stream("Not yet initialized to start an update.")
                 return
             self._loadAverage = list(os.getloadavg())
+            try:
+                self._nTasks = int(shell_command("ps auxwwH|wc -l"))
+            except:
+                traceback.print_exc()
+                self._nTasks = 0
+                
             #do in a separate thread to avoid to hang the device if it takes too long
             if self.Threaded:
                 if not self.updateThread == None and self.updateThread.is_alive():
@@ -455,13 +486,21 @@ class ProcessProfiler(PyTango.Device_4Impl):
             self.debug_stream('update_ProcessList(%s)' % self.UseEvents)
             startTime = time.time()
             #self._loadAverage = [] #list(os.getloadavg())
-            if self.UseEvents: self.fireEventsList([['LoadAverage',self._loadAverage]])
             #self._update_without_thread_split()#self._update_without_thread_split()
             self.update_all_processes()
             self.getMemRate()
             self.lapseTime = time.time()-startTime
             #self.info_stream('Update() finished in %6.3f seconds'%(self.lapseTime))
-            if self.UseEvents: self.fireEventsList([['UpdateLapseTime',self.lapseTime]])
+            if self.UseEvents: 
+                events = [('MemUsage',self.read_MemUsage()),
+                          ('LoadAverage',self._loadAverage),
+                          ('NTasks',self._nTasks),
+                          ('MemRate',self._memrate),
+                          ('MaxRss',self.maxrss),
+                          ('MaxRssProcess',self.maxrssname),
+                          ['UpdateLapseTime',self.lapseTime],
+                          ]                
+                self.fireEventsList(events)
             return
         except:
             self.error_stream('Unable to launch process list update:\n%s'%(traceback.format_exc()))
@@ -679,6 +718,14 @@ class ProcessProfilerClass(PyTango.PyDeviceClass):
                 'description': 'This attribute returns total-(free+cached) memory',
                 'unit':' ',
             } ],            
+       'NTasks':
+           [[PyTango.DevLong,
+           PyTango.SCALAR,
+           PyTango.READ],
+            {
+                'description': 'Number of Tasks/Threads on this host',
+                'unit':' ',
+            } ],                        
         }
 
     #Needed to have proper dynamic attributes
