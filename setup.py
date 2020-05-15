5ff5be08b9627d85af3190cc00fdf35cf5cf4e47
srubio <srubio@cells.es>
2020-05-15 12:25:52 +0200

    update launcher and setup

---
+++ setup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+__doc__ = """
+To install as system package:
+
+  python setup.py install
+  
+To install as local package:
+
+  RU=/opt/control
+  python setup.py egg_info --egg-base=tmp install --root=$RU/files --no-compile \
+    --install-lib=lib/python/site-packages --install-scripts=ds
+
+-------------------------------------------------------------------------------
+"""
+
+print(__doc__)
+
+version = open('VERSION').read().strip()
+scripts = ['ProcessProfiler']
+license = 'GPL-3.0'
+
+package_dir = {
+    'ProcessProfiler': '.',
+}
+packages = package_dir.keys()
+
+package_data = {
+    '': ['VERSION'],
+}
+
+packages = package_dir.keys()
+
+
+setup(name = 'processprofiler',
+      version = version,
+      license = license,
+      description = 'Tango device for collecting cpu and process stats',
+      packages = packages,
+      package_dir= package_dir,
+      scripts = scripts,
+      include_package_data = True,
+      package_data = package_data
+     )
